bcc596a27467631afd49b4635d5fd5a4cb60d498
Sergi SC <sersac78@hotmail.com>
2020-06-05 14:30:15 +0200

    5ena versió

--- insertar.php
+++ insertar.php
@@ -1,26 +1,32 @@
 <?php
 
-$band=$_POST['banda'];
-$titol=$_POST['titol'];
-$any=$_POST['any'];
+$band = $_POST['banda'];
+$titol = $_POST['titol'];
+$any = $_POST['any'];
+$missatgeError="Registre ja introduït";
+
 
 try {
-   
-    $connexio=new PDO("mysql:host=localhost;dbname=metalrecords","root","");
 
-    $connexio->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    if (!isset($_POST['titol'])) {
 
-    $connexio->exec("SET CHARACTER SET utf8");
+        $connexio = new PDO("mysql:host=localhost;dbname=metalrecords", "root", "");
 
-    $sql="INSERT INTO albums (nomBanda,titol, any) values (?,?,?)";
+        $connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $resultat=$connexio->prepare($sql);
+        $connexio->exec("SET CHARACTER SET utf8");
 
-    $resultat->execute(array($band,$titol,$any));
+        $sql = "INSERT INTO albums (nomBanda,titol, any) values (?,?,?)";
 
-    header("Location.options.php");
+        $resultat = $connexio->prepare($sql);
 
+        $resultat->execute(array($band, $titol, $any));
 
+        header("refresh:15;url=opcions.php");
+    } else {
+
+        echo $missatgeError;
+    }
 } catch (Exception $e) {
     die("Error" . $e->getMessage());
     echo " Hi ha un error  a la línia" . $e->getLine();
@@ -28,3 +34,36 @@ try {
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metal records</title>
+    <!--BOOTSTRAP CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+
+    <!-- CSS FILE-->
+    <link rel="stylesheet" href="css/main.css" />
+
+    <!--GOOGLE FONTS-->
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
+
+</head>
+
+<body>
+
+    <h1 style="text-align: center;margin-top:auto">L´album s´ha introduït correctament a la base de dades</h1>
+    <h2 style="text-align: center;margin-top:auto">Has introduït el següent registre:</h2>
+    <h3 style="text-align: center;margin-top:auto">Banda: <?php echo $band; ?></h3>
+    <h3 style="text-align: center;margin-top:auto">Títol: <?php echo $titol; ?></h3>
+    <h3 style="text-align: center;margin-top:auto">Any: <?php echo $any; ?></h3>
+    <footer>
+        Sergi Sánchez 2020 @Copyright
+    </footer>
+
+</body>
+
+</html>
--- registre.php
+++ registre.php
@@ -1,36 +1,65 @@
+<?php
 
-    <?php
+$mail = $_POST["email"];
+$password = $_POST["password"];
+$confirmarPass = $_POST["confirmarPass"];
 
-    $mail = $_POST["email"];
-    $password = $_POST["password"];
-    $confirmarPass = $_POST["confirmarPass"];
-
-    /*$pass_xifrat=password_hash($password,PASSWORD_DEFAULT);
+/*$pass_xifrat=password_hash($password,PASSWORD_DEFAULT);
     $confirmPass_xifrat=password_hash($confirmarPass,PASSWORD_DEFAULT);*/
 
+
+
+try {
+
     
+    $connexio = new PDO("mysql:host=localhost;dbname=metalrecords", "root", "");
 
-    try {
-        $connexio = new PDO("mysql:host=localhost;dbname=metalrecords", "root", "");
+    $connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexio->exec("SET CHARACTER SET utf8");
 
-        $connexio->exec("SET CHARACTER SET utf8");
+    $sql = "INSERT INTO users (email,password,confirmarPass) values(:mail,:password,:confirmarPass)";
 
-        $sql="INSERT INTO users (email,password,confirmarPass) values(:mail,:password,:confirmarPass)";
+    $resultat = $connexio->prepare($sql);
 
-        $resultat=$connexio->prepare($sql);
+    $resultat->execute(array(":mail" => $mail, ":password" => $password, ":confirmarPass" => $confirmarPass));
 
-        $resultat->execute(array(":mail"=>$mail,":password"=>$password,":confirmarPass"=>$confirmaPass));
+    header("refresh:15;url=opcions.php");
 
-        header("Location:index.html");
+    $resultat->closeCursor();
+} catch (Exception $e) {
 
-        $resultat->closeCursor();
+    die("Error" . $e->getMessage());
+    echo " Hi ha un error  a la línia" . $e->getLine();
+}
 
-    } catch (Exception $e) {
+?>
 
-        die("Error" . $e->getMessage());
-        echo " Hi ha un error  a la línia" . $e->getLine();
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!--BOOTSTRAP CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+
+    <!-- CSS FILE-->
+    <link rel="stylesheet" href="css/main.css" />
+
+    <!--GOOGLE FONTS-->
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+
+    <h1 style="text-align: center;margin-top:auto">Benvingut /da  <?php echo $mail; ?> .T´has donat d´alta correctament.En breu et redigirem a la plana d´opcions</h1>
+
+    <footer>
+        Sergi Sánchez 2020 @Copyright
+    </footer>
+</body>
+
+</html>
